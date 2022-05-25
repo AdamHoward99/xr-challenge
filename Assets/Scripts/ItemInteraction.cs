25a3edd44f1f730ab8161e0233e482e151f6d60b
AdamHoward99 <74617187+AdamHoward99@users.noreply.github.com>
2022-05-25 21:40:56 +0100

    Added SFX for collecting pickups

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class ItemInteraction : MonoBehaviour
 {
+    public AudioSource SFX;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Item")
         {
+            SFX.Play();
             ScoreSystem.UpdateScore(100);
             Destroy(other.gameObject);
         }
